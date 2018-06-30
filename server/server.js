810fc0d8119eb8bf05c73c76edb9f52fdad63541
Mohit Sharma <sharma_mohit@sharma.mohit>
2018-07-01 03:49:47 +0530

    Added basic socket client and server functions

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
-const publicPath = path.join(__dirname, '../public');
+var server = http.createServer(app);
+var io = socketIO(server);
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 })
