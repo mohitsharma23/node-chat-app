a72088d00c93ae435b7a3854631092580410fb02
Mohit Sharma <sharma_mohit@sharma.mohit>
2018-07-03 20:32:23 +0530

    Timestamps using momentJs

---
+++ playground/time.js
@@ -0,0 +1,15 @@
+const moment = require('moment');
+
+// var date = new Date();
+// console.log(date.getMonth());
+
+// var date = moment();
+// console.log(date.format('MMM Do, YYYY'));
+
+// new Date().getTime()
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
